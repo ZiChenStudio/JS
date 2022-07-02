b4993bf868419f9ea9e653b3467f9924d8e9890d
ZiChenStudio Officia <105914059+ZiChenStudio@users.noreply.github.com>
2022-07-02 19:23:03 +0800

    Create translate.js

---
+++ translate.js
@@ -0,0 +1,44 @@
+function English()
+{
+    x=document.getElementById("F1");
+    x.innerHTML="ZiChen Official Website";
+
+    x=document.getElementById("F2");
+    x.innerHTML="This is ZiChen official website";
+
+    x=document.getElementById("F3");
+    x.innerHTML="Notice:We are a tech organization";
+
+    x=document.getElementById("F4");
+    x.innerHTML="You can find us at the following website";
+
+    x=document.getElementById("F5");
+    x.innerHTML="ZiChen Official website";
+
+    x=document.getElementById("F6");
+    x.innerHTML="The following is the software independently produced by ZiChen";
+
+    x=document.getElementById("F7");
+    x.innerHTML="PVZ-ZS all versions";
+
+    x=document.getElementById("F8");
+    x.innerHTML="Prohibit boot for a period of time after shutdown (time can be set freely)";
+
+    x=document.getElementById("F9");
+    x.innerHTML="Aircraft Wars (password: ddw2)";
+
+    x=document.getElementById("F10");
+    x.innerHTML="About ZiChen";
+
+    x=document.getElementById("F11");
+    x.innerHTML="ZiChen readme";
+
+    x=document.getElementById("F12");
+    x.innerHTML="ZiChen Calendar";
+
+    x=document.getElementById("F13");
+    x.innerHTML="ZiChen Calendar";
+
+    x=document.getElementById("F14");
+    x.innerHTML="Back"
+}
