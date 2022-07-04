e71be4efbb7ec04efc73115cebff15976d62092c
ZiChenStudio Officia <105914059+ZiChenStudio@users.noreply.github.com>
2022-07-04 19:12:16 +0800

    Create translate.js

--- translate.js
+++ translate.js
@@ -13,32 +13,50 @@ function English()
     x.innerHTML="You can find us at the following website";
 
     x=document.getElementById("F5");
-    x.innerHTML="ZiChen Official website";
+    x.innerHTML="BiliBili";
 
     x=document.getElementById("F6");
-    x.innerHTML="The following is the software independently produced by ZiChen";
+    x.innerHTML="ZiChen Official website";
 
     x=document.getElementById("F7");
-    x.innerHTML="PVZ-ZS all versions";
+    x.innerHTML="GitHub";
 
     x=document.getElementById("F8");
-    x.innerHTML="Prohibit boot for a period of time after shutdown (time can be set freely)";
+    x.innerHTML="The following is the software independently produced by ZiChen";
 
     x=document.getElementById("F9");
-    x.innerHTML="Aircraft Wars (password: ddw2)";
+    x.innerHTML="PVZ-ZS all versions";
 
     x=document.getElementById("F10");
-    x.innerHTML="About ZiChen";
+    x.innerHTML="Prohibit boot for a period of time after shutdown (time can be set freely)";
 
     x=document.getElementById("F11");
-    x.innerHTML="ZiChen readme";
+    x.innerHTML="Aircraft Wars (password: ddw2)";
 
     x=document.getElementById("F12");
-    x.innerHTML="ZiChen Calendar";
+    x.innerHTML="About ZiChen";
 
     x=document.getElementById("F13");
-    x.innerHTML="ZiChen Calendar";
+    x.innerHTML="ZiChen readme";
 
     x=document.getElementById("F14");
+    x.innerHTML="ZiChen Calendar";
+
+    x=document.getElementById("F15");
+    x.innerHTML="ZiChen Calendar";
+
+    x=document.getElementById("F16");
     x.innerHTML="Back"
+
+    x=document.getElementById("F17");
+    x.innerHTML="ZiChen PVZ-ZS official website"
+
+    x=document.getElementById("F18");
+    x.innerHTML="Still being updated"
+
+    x=document.getElementById("F19");
+    x.innerHTML="The version number with the t after it is the test version"
+
+    x=document.getElementById("F20");
+    x.innerHTML="Our official website"
 }
