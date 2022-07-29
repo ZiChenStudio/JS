a21f08ad72dac70746c9bdaa093408a078c9d9ca
ZiChenStudio Officia <105914059+ZiChenStudio@users.noreply.github.com>
2022-07-29 15:31:00 +0800

    Create tips.js

---
+++ tips.js
@@ -0,0 +1,13 @@
+function Tips(){
+    var x;
+    var r=confirm('点击同样代表您同样了我们的免责声明 \n如果您点击了取消代表您拒绝我们的免责声明 \n免责声明：https://zichenstudio.github.io');
+    if (r==true){
+        x="你按下了\"确定\"按钮!";
+    }
+    else{
+        window.opener = null;
+        window.open('', '_self');
+        window.close()
+    }
+    document.getElementById("demo").innerHTML=x;
+    }
