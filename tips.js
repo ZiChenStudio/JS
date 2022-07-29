ce5b58bd1a38de6eac82b2780385cde0dde86ae6
ZiChenStudio Officia <105914059+ZiChenStudio@users.noreply.github.com>
2022-07-29 15:33:15 +0800

    Update tips.js

--- tips.js
+++ tips.js
@@ -1,6 +1,6 @@
 function Tips(){
     var x;
-    var r=confirm('点击同样代表您同样了我们的免责声明 \n如果您点击了取消代表您拒绝我们的免责声明 \n免责声明：https://zichenstudio.github.io/readme.html');
+    var r=confirm('点击同样代表您同样了我们的免责声明 \n如果您点击了取消代表您拒绝我们的免责声明 \n免责声明：https://zichenstudio.github.io/readme.html ');
     if (r==true){
         x="你按下了\"确定\"按钮!";
     }
